daa73bce9df67287c8c82d465279bdbe86688a78
Kristofer Munsterhjelm <kristofer@munsterhjelm.no>
2019-04-24 19:26:03 +0200

    Revert test_generator to master in preparation for ISDA reimplementation.

--- src/singlewinner/brute_force/general_rpn/composition/test_generator.cc
+++ src/singlewinner/brute_force/general_rpn/composition/test_generator.cc
@@ -1,6 +1,5 @@
 #include "test_generator.h"
 #include "vector_ballot.h"
-#include "isda_reduction.h"
 
 #include "../../../../linear_model/constraints/numvoters.h"
 #include "../../../../linear_model/constraints/pairwise.h"
@@ -74,14 +73,10 @@ std::pair<constraint_set, bool> test_generator::set_scenario_constraints(
 	all_constraints.add(rel_criterion.relative_constraints(before_name,
 		after_name));
 
-	// Add the definition of a minimum margin (required for pairwise
-	// constraints).
-	// TODO: Make whoever it is that's responsible for fixed params detect
-	// if this isn't done.
+	// Set the margin of pairwise victory and add a nonnegativity
+	// constraint.
 	all_constraints.set_fixed_param("min_victory_margin", 0.01);
-
-	// Add a nonnegativity constraint. (HACK: Done elsewhere to avoid
-	// overlapping constraints.)
+	all_constraints.add(general_const::all_nonnegative(all_constraints));
 
 	out.first = all_constraints;
 	out.second = true;
@@ -93,97 +88,13 @@ bool test_generator::set_scenarios(copeland_scenario before,
 	copeland_scenario after, int max_numvoters,
 	const relative_criterion_const & rel_criterion) {
 
-	// Handle ISDA. There are three possible situations:
-	//	- Both before and after have full Smith sets, nothing needs be done.
-	//	- Before has a full Smith set and after does not: ISDA comes at the
-	//		end
-	//	- After has a full Smith set and before does not: ISDA comes at the
-	//		beginning.
+	// Set up constraints. (This has been separated out to better deal
+	// with composition required by ISDA later.)
 
-	isda_reduction isda;
-	if (!isda.set_scenario_constraints(before, after, "before_isda",
-			"after_isda")) {
-		// If it returns false, the scenario combination doesn't need to be
-		// tested.
-		return false;
-	}
-
-	std::string before_scenario_name = "before",
-				after_scenario_name = "after";
-
-	std::pair<constraint_set, bool> constraints;
-
-	isda_first = false;
-	isda_last = false;
-
-	if (isda.is_interposing_required()) {
-
-		// It shouldn't happen that both or neither of these are set.
-		assert (isda.alters_before ^ isda.alters_after);
-
-		if (isda.alters_after) {
-			// before -> mono-raise -> before_isda -> after_isda
-			isda_last = true;
-			constraints = set_scenario_constraints(
-				before, after, max_numvoters, rel_criterion, "before",
-				"before_isda");
-			before_scenario_name = "before";
-			after_scenario_name = "after_isda";
-
-			// TODO? Move into isda_reduction.
-			for (size_t before_cddt = 0; before_cddt < before.get_numcands();
-				++before_cddt) {
-				if (isda.elimination_spec[before_cddt] != -1) {
-					elim_permutations.push_back(std::pair<size_t, size_t>(
-						before_cddt, isda.elimination_spec[before_cddt]));
-				}
-			}
-		}
-
-		if (isda.alters_before) {
-			// before_isda -> after_isda -> mono-raise -> after
-			isda_first = true;
-			constraints = set_scenario_constraints(
-				before, after, max_numvoters, rel_criterion, "after_isda",
-				"after");
-			before_scenario_name = "before_isda";
-			after_scenario_name = "after";
-
-			for (size_t after_cddt = 0; after_cddt < after.get_numcands();
-				++after_cddt) {
-				if (isda.elimination_spec[after_cddt] != -1) {
-					elim_permutations.push_back(std::pair<size_t, size_t>(
-						isda.elimination_spec[after_cddt], after_cddt));
-				}
-			}
-		}
-
-		before = isda.inner_before;
-		after = isda.inner_after;
-
-		/*std::cout << "DEBUG GMPL only isda constraints" << std::endl;
-		isda.interposing_constraints.print_gmpl_program();
-		std::cout << "All done..." << std::endl;*/
-
-		constraints.first.add(isda.interposing_constraints);
-		/*std::cout << "DEBUG GMPL" << std::endl;
-		constraints.first.print_gmpl_program();
-		std::cout << std::endl;*/
-	} else {
-		// Set up constraints without any need for ISDA.
-		constraints = set_scenario_constraints(
-			before, after, max_numvoters, rel_criterion,
-			before_scenario_name, after_scenario_name);
-	}
+	std::pair<constraint_set, bool> constraints = set_scenario_constraints(
+		before, after, max_numvoters, rel_criterion, "before", "after");
 
 	if (!constraints.second) { return false; }
-
-	constraints.first.add(general_const::all_nonnegative(constraints.first));
-
-	// Done setting up constraints.
-
-	numcands_before = before.get_numcands();
-	numcands_after = after.get_numcands();
 
 	// Set up the polytope and sampler. Return true if we can do so,
 	// false if we can't.
@@ -198,11 +109,9 @@ bool test_generator::set_scenarios(copeland_scenario before,
 	// Set the lookup permutations we need in order to return ballots.
 
 	before_permutation_indices = election_polytope.
-		get_all_permutations_indices(before.get_numcands(),
-		before_scenario_name);
+		get_all_permutations_indices(before.get_numcands(), "before");
 	after_permutation_indices = election_polytope.
-		get_all_permutations_indices(after.get_numcands(),
-		after_scenario_name);
+		get_all_permutations_indices(after.get_numcands(), "after");
 
 	scenario_before = before;
 	scenario_after = after;
@@ -212,8 +121,8 @@ bool test_generator::set_scenarios(copeland_scenario before,
 
 relative_test_instance test_generator::sample_instance(
 	size_t other_candidate_idx_before, size_t other_candidate_idx_after,
-	const fixed_cand_equivalences & before_cand_remapping,
-	const fixed_cand_equivalences & after_cand_remapping) {
+	const fixed_cand_equivalences before_cand_remapping,
+	const fixed_cand_equivalences after_cand_remapping) {
 
 	Eigen::VectorXd point = sampler.billiard_walk();
 
@@ -281,48 +190,4 @@ relative_test_instance test_generator::sample_instance(
 	out.after_B.scenario = cand_remapping.to_scenario;
 
 	return out;
-}
-
-// TODO: fix access to reference criterion...
-// Maybe it's better to just iterate over all triples...
-bool test_generator::permitted_b_pair(size_t b_before, size_t b_after,
-	const relative_criterion_const & rel_criterion) const {
-
-	// Go through every allowed pair, and transform according to
-	// relative_criterion.
-
-	// Currently a very ugly hack is used. This should really be handled
-	// when generating the pairs.
-
-	if (!isda_first && !isda_last) {
-		if (rel_criterion.get_before_cand_number(b_after) != b_before) {
-			return false;
-		}
-		return true;
-	}
-
-	if (isda_first) {
-		for (const std::pair<size_t, size_t> & permitted_pair:
-			elim_permutations) {
-			if (permitted_pair.first != b_before) { continue; }
-			if (rel_criterion.get_before_cand_number(b_after) !=
-				permitted_pair.second) { continue; }
-
-			return true;
-		}
-	}
-
-	if (isda_last) {
-		for (const std::pair<size_t, size_t> & permitted_pair:
-			elim_permutations) {
-
-			if (permitted_pair.second != b_after) { continue; }
-			if (rel_criterion.get_before_cand_number(permitted_pair.first) !=
-				b_before) { continue; }
-
-			return true;
-		}
-	}
-
-	return false;
 }
--- src/singlewinner/brute_force/general_rpn/composition/test_instance_gen.cc
+++ src/singlewinner/brute_force/general_rpn/composition/test_instance_gen.cc
@@ -15,7 +15,7 @@ relative_test_instance test_instance_generator::get_test_instance(
 	return ti;
 }
 
-// Where should I put it?
+// Cut and paste code, fix later! But where should I put it?
 std::vector<test_instance_generator> get_all_permitted_test_generators(
 	double max_numvoters,
 	const std::vector<copeland_scenario> canonical_scenarios,
@@ -25,16 +25,6 @@ std::vector<test_instance_generator> get_all_permitted_test_generators(
 
 	std::vector<test_instance_generator> out;
 
-	// There are two steps to this function. First, we create all the test
-	// generators based on what the relative criterion will permit. During
-	// creation, the test generator might invoke ISDA wrappers or similar,
-	// so we don't know what the scenarios will actually *be*.
-
-	// Then, we create every allowed test instance generator based on those
-	// test generators.
-
-	//
-
 	// Must get before and after numcands for the relative criterion,
 	// then go through canonical scenarios for those candidate numbers.
 	// The inner loop should go through every (before cand, after cand)
@@ -43,7 +33,9 @@ std::vector<test_instance_generator> get_all_permitted_test_generators(
 	size_t numcands_before = relative_criterion.get_numcands_before(),
 		numcands_after = relative_criterion.get_numcands_after();
 
-	std::vector<test_generator> generators;
+	fixed_cand_equivalences before_cand_remapping =
+		cand_equivs.find(numcands_before)->second, after_cand_remapping =
+		cand_equivs.find(numcands_after)->second;
 
 	for (copeland_scenario x: canonical_scenarios) {
 		if (x.get_numcands() != numcands_before) { continue; }
@@ -52,8 +44,8 @@ std::vector<test_instance_generator> get_all_permitted_test_generators(
 			if (y.get_numcands() != numcands_after) { continue; }
 			test_generator cur_test(randomizer.long_rand());
 
-			std::cout << "Combination " << relative_criterion.name()
-				<< ", " << x.to_string() << ", " << y.to_string() << ": ";
+			std::cout << "Combination " << x.to_string() << ", "
+				<< y.to_string() << ":";
 			if (!cur_test.set_scenarios(x, y, max_numvoters,
 				relative_criterion)) {
 				std::cout << "not permitted\n";
@@ -61,57 +53,44 @@ std::vector<test_instance_generator> get_all_permitted_test_generators(
 			}
 
 			std::cout << "permitted\n";
-			generators.push_back(cur_test);
-		}
-	}
 
-	for (test_generator & gen: generators) {
+			for (size_t before_cand_idx = 1;
+				before_cand_idx < numcands_before; ++before_cand_idx) {
 
-		// Very ugly hack. We'll fix this later by making it possible to
-		// enumerate the permitted candidate pairs. (??)
-		size_t gen_numcands_before = gen.numcands_before,
-				gen_numcands_after = gen.numcands_after;
+				for (size_t after_cand_idx = 0;
+					after_cand_idx < numcands_after; ++after_cand_idx) {
 
-		fixed_cand_equivalences before_cand_remapping = cand_equivs.find(
-			gen_numcands_before)->second, after_cand_remapping =
-			cand_equivs.find(gen_numcands_after)->second;
+					// Only continue with the permitted pairs.
+					if (relative_criterion.get_before_cand_number(
+						after_cand_idx) != before_cand_idx) {
+						continue;
+					}
 
-		for (size_t before_cand_idx = 1;
-			before_cand_idx < gen_numcands_before; ++before_cand_idx) {
+					test_instance_generator to_add(cur_test);
+					// Set a different seed but use the same sampler and
+					// polytope as we created earlier.
+					to_add.tgen.set_rng_seed(randomizer.long_rand());
 
-			for (size_t after_cand_idx = 0;
-				after_cand_idx < gen_numcands_after; ++after_cand_idx) {
+					// Set what kind of test this group should perform,
+					// depending on what the relative criterion says.
 
-				// Only continue with the permitted pairs.
-				if (!gen.permitted_b_pair(before_cand_idx,
-					after_cand_idx, relative_criterion)) {
-					continue;
+					to_add.no_help = relative_criterion.no_help();
+					to_add.no_harm = relative_criterion.no_harm();
+
+					// Get the scenarios by sampling once.
+					relative_test_instance ti = to_add.tgen.sample_instance(
+						before_cand_idx, after_cand_idx,
+						before_cand_remapping, after_cand_remapping);
+
+					to_add.before_A = ti.before_A.scenario;
+					to_add.before_B = ti.before_B.scenario;
+					to_add.after_A = ti.after_A.scenario;
+					to_add.after_B = ti.after_B.scenario;
+
+					to_add.cand_B_idx_before = before_cand_idx;
+					to_add.cand_B_idx_after = after_cand_idx;
+					out.push_back(to_add);
 				}
-
-				test_instance_generator to_add(gen);
-				// Set a different seed but use the same sampler and
-				// polytope as we created earlier.
-				to_add.tgen.set_rng_seed(randomizer.long_rand());
-
-				// Set what kind of test this group should perform,
-				// depending on what the relative criterion says.
-
-				to_add.no_help = relative_criterion.no_help();
-				to_add.no_harm = relative_criterion.no_harm();
-
-				// Get the scenarios by sampling once.
-				relative_test_instance ti = to_add.tgen.sample_instance(
-					before_cand_idx, after_cand_idx,
-					before_cand_remapping, after_cand_remapping);
-
-				to_add.before_A = ti.before_A.scenario;
-				to_add.before_B = ti.before_B.scenario;
-				to_add.after_A = ti.after_A.scenario;
-				to_add.after_B = ti.after_B.scenario;
-
-				to_add.cand_B_idx_before = before_cand_idx;
-				to_add.cand_B_idx_after = after_cand_idx;
-				out.push_back(to_add);
 			}
 		}
 	}
