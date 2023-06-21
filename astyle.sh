6ea3f4e69913d59cefc994dcfd3b9e77e073e090
Kristofer Munsterhjelm <kristofer@munsterhjelm.no>
2023-06-21 20:08:15 +0200

    Remove some unused/incorrect GNU project files, and tweak astyle.sh.

--- astyle.sh
+++ astyle.sh
@@ -5,4 +5,7 @@
 # put braces around one-liners, indent classes and case switches,
 # and keep one-line statements and if spacing to parens.
 
-astyle -T -A2 -j -o --mode=c -xC75 -W2 -C -S -U -xU -H $*
+# Preserve the creation date, don't store an .orig backup, and only
+# output the names of files that were formatted.
+
+astyle -T -A2 -j -o --mode=c -xC75 -W2 -C -S -U -xU -H -Z --suffix=none -Q $*
