8ecc2336f117ebe344b31095b36d943e0c2a6f40
Kristofer Munsterhjelm <kristofer@munsterhjelm.no>
2024-02-29 18:09:55 +0100

    RMR1: Remove debug output, and clean up code and explanations.

--- src/singlewinner/experimental/rmr1.cc
+++ src/singlewinner/experimental/rmr1.cc
@@ -35,18 +35,13 @@ beats_tensor rmr1::get_k_disqualifications(const election_t & papers,
 
 	for (const std::vector<bool> & candidate_selection: hopeful_power_set) {
 
-		std::cout << "RMR1: Debug: handling set: ";
-
 		int num_remaining_candidates_here = 0;
 
 		for (size_t i = 0; i < candidate_selection.size(); ++i) {
 			if (candidate_selection[i]) {
 				++num_remaining_candidates_here;
-				std::cout << (char)(i + 'A') << " ";
 			}
 		}
-
-		std::cout << " cardinality " << num_remaining_candidates_here << std::endl;
 
 		ordering plur_outcome;
 
@@ -64,9 +59,6 @@ beats_tensor rmr1::get_k_disqualifications(const election_t & papers,
 			first_prefs_this_selection[cand_and_score.get_candidate_num()] =
 				cand_and_score.get_score();
 			num_remaining_voters_here += cand_and_score.get_score();
-
-			std::cout << cand_and_score.get_candidate_num() << " scored " <<
-				cand_and_score.get_score() << std::endl;
 		}
 
 		first_pref_scores.push_back(first_prefs_this_selection);
@@ -83,8 +75,9 @@ beats_tensor rmr1::get_k_disqualifications(const election_t & papers,
 
 			// If we're not disqualifying anyone, then we need to set
 			// the disqualification relation to every other candidate
-			// to false. If we are above the threshold, then we don't
-			// need to do anything.
+			// to false. In other words, if we're below or at the 1/k threshold,
+			// then we can't be disqualifying anyone, so set every
+			// disqualification by us of this cardinality to false.
 
 			if (num_remaining_candidates_here * first_prefs_this_selection[cand] >
 				num_remaining_voters_here) {
@@ -98,10 +91,6 @@ beats_tensor rmr1::get_k_disqualifications(const election_t & papers,
 				if (other_cand == cand) {
 					continue;
 				}
-
-				std::cout << "Cardinality " << num_remaining_candidates_here <<
-					": Noting that " << cand << " is not disqualifying " << other_cand <<
-					std::endl;
 
 				beats[num_remaining_candidates_here][cand][other_cand] = false;
 			}
@@ -160,34 +149,30 @@ std::pair<ordering, bool> rmr1::elect_inner(
 		// And assume we're defeated.
 		bool defeated = true;
 
-		std::cout << "Making the case for " << cand << std::endl;
-
 		for (int k = 2; k <= num_candidates && defeated; ++k) {
 			// Check if there are any defeats against us that we
-			// can erase, and check if we're undefeated in the same
-			// go.
+			// can erase, and check if that makes us undefeated.
+
 			defeated = false;
 
-			std::cout << "\tk=" << k << std::endl;
+			// NOTE: We can *not* short-circuit here because we need
+			// dto erase every defeat that can be erased, even though
+			// we might already know we're defeated by someone else.
 
 			for (size_t challenger = 0; challenger < (size_t)num_candidates;
 				++challenger) {
+
 				if (!hopefuls[challenger] || challenger == cand) {
 					continue;
 				}
 
-				if (beats[k][cand][challenger] || !beats[k][challenger][cand]) {
-					std::cout << "Found a disqualification by " << cand << " over " <<
-						challenger << std::endl;
+				// Is the disqualification against us no longer active?
+				// Break it.
+				if (!beats[k][challenger][cand]) {
 					current_beats[challenger][cand] = false;
 				}
 
 				defeated |= current_beats[challenger][cand];
-
-				if (current_beats[challenger][cand]) {
-					std::cout << "detected a beat by " << challenger << " over " << cand <<
-						std::endl;
-				}
 			}
 
 			if (!defeated) {
--- src/singlewinner/experimental/rmr1.h
+++ src/singlewinner/experimental/rmr1.h
@@ -7,20 +7,22 @@
 // First set the defeat graph so that a candidate has an edge pointed at
 // him if someone else beats him pairwise.
 
-// Then do the following for k = 3...n.
+// Then do the following for k = 3...n:
 
-// For every candidate B who is defeating A according to the graph,
-// check if A ~(k)~> B, i.e. if A disqualifies B on all sets of cardinality
-// k. If so, reverse the direction of the edge between A and B.
+// 		For every candidate B who is defeating A according to the graph,
+//		check if B no longer disqualifies A on every set of cardinality k
+//		or less. If so, flip the direction of the defeat B>A on the graph.
 
-// If A is now undefeated, return A's penalty as k. Otherwise, if k=n,
-// return A's penalty as infinity (or n+1).
+//		If A is now undefeated, return A's penalty as k. Otherwise, increase
+//		the cardinality by one if possible, but if k=n, return A's penalty
+//		as infinity (or n+1).
 
 // The candidate with the least penalty wins. This method might not be
-// resolvable; I'll check that later.
+// resolvable; I'll check that later. It doesn't provide a social order,
+// just a winning set.
 
-// A ~(k)~>B is defined as by the disqualification relation of the
-// resistant/inner burial set.
+// The disqualification relation is the same as for the resistant/inner
+// burial set.
 
 // A lot of this is stolen from the resistant set code; I'll refactor
 // later if this seems to show promise. TODO.
@@ -46,7 +48,7 @@ class rmr1 : public election_method {
 			bool winner_only) const;
 
 		std::string name() const {
-			return "RMR1 (EXP)";
+			return "EXP: RMR1";
 		}
 
 		// I'm not sure which is better; IFPP Method X uses
