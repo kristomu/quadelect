0ea2a323c741f9e3a87860eee1c0a2519796535c
Kristofer Munsterhjelm <kristofer@munsterhjelm.no>
2025-01-30 19:55:41 +0100

    Remove tournament method, as I'm not sure it works.

--- src/singlewinner/pairwise/all.h
+++ src/singlewinner/pairwise/all.h
@@ -1,9 +1,6 @@
 
 // All pairwise methods. Use this if you want to include the whole batch.
 
-#ifndef _VOTE_SW_PAIRWISE
-#define _VOTE_SW_PAIRWISE
-
 #include "keener.h"
 #include "least_rev.h"
 #include "rpairs.h"
@@ -12,10 +9,7 @@
 #include "kemeny.h"
 #include "method.h"
 #include "simple_methods.h"
-#include "tournament.h"
 #include "minmaxy_exper.h"
 
 #include "odm/all.h"
 #include "random/all.h"
-
-#endif
