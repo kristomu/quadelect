56100e1f3d4a9824e769d78e07891474ab516d29
Kristofer Munsterhjelm <kristofer@munsterhjelm.no>
2025-06-09 19:18:41 +0200

    Extract out subelections calculations for the resistant set.

--- src/singlewinner/dmt/README.md
+++ src/singlewinner/dmt/README.md
@@ -3,3 +3,5 @@ candidate burial resistance. These could possibly be used in combination with
 Condorcet and Smith prefixes to produce monotone low-burial election methods.
 
 Most are monotone; any non-monotone method will have comments saying as much.
+
+The subdirectory resistant/ contains code for calculating subelection results. These results are used to determine the resistant set and for methods based on it.
---
+++ src/singlewinner/dmt/resistant/subelections.h
@@ -0,0 +1,42 @@
+#pragma once
+
+#include "singlewinner/positional/simple_methods.h"
+
+// A subelection, given by a set S, is an election with every candidate not
+// in S eliminated. This class counts the first preferences for every
+// subelection of a given election. The first preference information is used
+// in resistant set-based methods to determine or elect from the resistant set.
+
+class subelections {
+	private:
+		plurality plurality_method;
+
+	public:
+		// The hopeful power set lists the candidates included in each
+		// subelection, while the first_pref_scores list their first
+		// preferences, and the num_remaining_candidates and num_remaining
+		// voters vectors list how many candidates are included in the
+		// subelection and how many continuing (non-exhausted) voters are
+		// involved.
+
+		std::vector<std::vector<bool > > hopeful_power_set;
+		std::vector<std::vector<double> > first_pref_scores;
+		std::vector<int> num_remaining_candidates;
+		std::vector<double> num_remaining_voters;
+
+		// The hopefuls that make up the full election.
+		std::vector<bool> included_candidates;
+		size_t num_candidates; // including already eliminated (non-hopefuls)
+
+		// Determine the subelection first-preference counts for
+		// the given election. If tiebreak is true, add a small
+		// value to lower-numbered candidates so that true ties
+		// never occur. (Doing so breaks neutrality.)
+		void count_subelections(const election_t & papers,
+			const std::vector<bool> & hopefuls,
+			bool tiebreak);
+
+		// I'm not sure which is better; IFPP Method X uses
+		// whole, and this uses fractional.
+		subelections() : plurality_method(PT_FRACTIONAL) {}
+};
